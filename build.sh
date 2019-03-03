96a3bfd50241321499b6178b90777ce6b8fa799a
Ferenc- <ferenc.gm@gmail.com>
2019-03-03 20:12:33 +0100

    Add initial recompilation of curl and a crude amtc build

---
+++ build.sh
@@ -0,0 +1,45 @@
+pushd /tmp
+    git clone https://github.com/schnoddelbotz/amtc
+popd
+
+ls -lah /tmp/amtc/src/
+
+# https://openwrt.org/docs/guide-developer/using_the_sdk
+
+docker pull rychly/openwrt-sdk:18.06.2-ar71xx
+
+docker run --name openwrt-sdk -v ${PWD}:${PWD} -v /tmp/amtc:/tmp/amtc --workdir ${PWD} rychly/openwrt-sdk:18.06.2-ar71xx
+
+# Vim is needed for the xxd tool
+apk add vim git
+
+cd $SDK_HOME
+./scripts/feeds update -a
+./scripts/feeds install curl
+cd -
+
+cp ${PWD}/.config $SDK_HOME/.config
+cd -
+## This config is normally created by the menuconfig
+##echo 'CONFIG_PACKAGE_libcurl=m' >> .config
+#echo 'CONFIG_LIBCURL_GNUTLS=y' >> .config
+#echo 'CONFIG_LIBCURL_CRYPTO_AUTH=y' >> .config
+
+
+make V=99 package/curl/compile
+
+
+cp feeds.conf.default feeds.conf
+cd -
+echo "src-link mypackages ${PWD}/mypackages" >> $SDK_HOME/feeds.conf
+cd $SDK_HOME
+./scripts/feeds update mypackages
+./scripts/feeds install amtc
+
+# Normally you would have to do menuconfig here as well
+# echo 'CONFIG_PACKAGE_amtc=m' >> .config
+
+make V=99 package/amtc/compile
+
+
+ls -lah ./staging_dir/target-mips_24kc_musl/root-ar71xx/usr/bin/amtc
